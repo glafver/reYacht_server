7eaf13c43826298915e95e22565bfe0252de2331
Adi Dzocaj <adidzocaj@gmail.com>
2022-05-25 10:44:08 +0200

    Changed from filtering to mapping out the coordinates instead, seemed more appropriate.

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -278,13 +278,12 @@ module.exports = function (socket, _io) {
 				// Map out other users yachts
 				item.yachts.map((yacht) => {
 					// debug(yacht)
-					// Filter out rows and columns ( coordinates ) of the yachts aswell as their index in the array
-					yacht.points.filter((coordinate, index) => {
+					// map out the rows and columns ( coordinates ) of the yachts aswell as their index in the array
+					yacht.points.map((coordinate, index) => {
 						// Check if the coordinates of the shootTarget match any of the other users yacht coordinates and if so, splice it from the array and push it in hitYachtCoordinate array, if not, return nothing
 						if (coordinate.row === shootTarget.row && coordinate.col === shootTarget.col) {
 							
 							let hit = "that was a hit"
-							debug(coordinate.row, coordinate.col)
 							let rowCor = coordinate.row+1
 							let colCor = coordinate.col+1
 							io.to(user.id).emit('shot:hit', hit, rowCor, colCor)
